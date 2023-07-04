3ad7bd282d3f6edded6df9f374f1a05e2afe3ea3
suxiaorui <aaa1097395576@foxmail.com>
2023-07-04 22:15:53 +0800

    整合并且测试了redis

--- service-user/src/main/java/com/rui/user/controller/HelloController.java
+++ service-user/src/main/java/com/rui/user/controller/HelloController.java
@@ -2,8 +2,8 @@ package com.rui.user.controller;
 
 import com.rui.api.controller.user.HelloControllerApi;
 import com.rui.grace.result.GraceJSONResult;
-import com.rui.grace.result.ResponseStatusEnum;
 import com.rui.grace.result.RuiJSONResult;
+import com.rui.utils.RedisOperator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,14 +22,15 @@ public class HelloController implements HelloControllerApi {
 
     final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    @Autowired
+    private RedisOperator redis;
 
     //    Swagger2 文档生成工具
-    public Object hello() {
-
+    public Object hello()  {
         logger.debug("debug: hello~");
-        logger.info("info: hello~");
-        logger.warn("warn: hello~");
-        logger.error("error: hello~");
+//        logger.info("info: hello~");
+//        logger.warn("warn: hello~");
+//        logger.error("error: hello~");
 
 //        return "hello";
         return RuiJSONResult.ok();
@@ -37,5 +38,11 @@ public class HelloController implements HelloControllerApi {
 //        return RuiJSONResult.errorMsg("您的信息有误~！");
 //        return GraceJSONResult.errorCustom(ResponseStatusEnum.NO_AUTH);
 
+    }
+
+    @GetMapping("/redis")
+    public Object redis(){
+        redis.set("age","18");
+        return GraceJSONResult.ok(redis.get("age"));
     }
 }
