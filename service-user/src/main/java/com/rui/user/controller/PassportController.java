4ee90d0f00f27ccda6e3ea39e8fe219eb310e593
suxiaorui <aaa1097395576@foxmail.com>
2023-07-04 22:43:51 +0800

    解决跨域问题

--- service-user/src/main/java/com/rui/user/controller/PassportController.java
+++ service-user/src/main/java/com/rui/user/controller/PassportController.java
@@ -1,12 +1,13 @@
 package com.rui.user.controller;
 
+import com.rui.api.BaseController;
 import com.rui.api.controller.user.PassportControllerApi;
 import com.rui.grace.result.GraceJSONResult;
+import com.rui.utils.IPUtil;
 import com.rui.utils.SMSUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,22 +20,35 @@ import javax.servlet.http.HttpServletRequest;
  */
 
 @RestController
-public class PassportController implements PassportControllerApi  {
+public class PassportController extends BaseController implements PassportControllerApi  {
 
     final static Logger logger = LoggerFactory.getLogger(PassportController.class);
 
     @Autowired
     private SMSUtils smsUtils;
 
+
     @Override
     public GraceJSONResult getSMSCode(String mobile, HttpServletRequest request) {
 
-        String code = "123456";
-        try {
-            smsUtils.sendSMS(mobile,code);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //获得用户ip
+        String userIp = IPUtil.getRequestIp(request);
+
+        // 根据用户的ip进行限制，限制用户在60s内只能获得一次验证码
+        redis.setnx60s(MOBILE_SMSCODE + ":" + userIp, userIp);
+
+        //生成随机验证码并且发送短信
+        String random = (int)((Math.random() * 9 + 1) * 100000) + "";
+
+//        try {
+//            smsUtils.sendSMS(mobile,random);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        // 将验证码存入redis，用于后续验证
+        redis.set(MOBILE_SMSCODE + ":" + mobile, random,30 * 60);
+
         return GraceJSONResult.ok();
     }
 }
