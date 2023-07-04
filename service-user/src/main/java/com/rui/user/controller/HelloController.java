b285224da961fc5d0f507f501f046d2c8d686c5c
suxiaorui <aaa1097395576@foxmail.com>
2023-07-04 18:00:48 +0800

    配置了logback日志和多环境profile

--- service-user/src/main/java/com/rui/user/controller/HelloController.java
+++ service-user/src/main/java/com/rui/user/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.rui.user.controller;
 
 import com.rui.api.controller.user.HelloControllerApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController implements HelloControllerApi {
 
-    public Object hello(){
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
+    //    Swagger2 文档生成工具
+    public Object hello() {
+
+        logger.debug("debug: hello~");
+        logger.info("info: hello~");
+        logger.warn("warn: hello~");
+        logger.error("error: hello~");
+
         return "hello";
+//        return IMOOCJSONResult.ok();
+//        return IMOOCJSONResult.ok("hello");
+//        return IMOOCJSONResult.errorMsg("您的信息有误~！");
 
     }
 }
