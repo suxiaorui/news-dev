bbda1ff259593ec1bf4665fa0644e277b44c18a7
suxiaorui <aaa1097395576@foxmail.com>
2023-07-13 15:27:50 +0800

    实现了多文件上传、发布文章入库等功能

---
+++ service-article/src/main/java/com/rui/article/Application.java
@@ -0,0 +1,17 @@
+package com.rui.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.rui.article.mapper")
+@ComponentScan(basePackages = {"com.rui", "org.n3r.idworker"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
