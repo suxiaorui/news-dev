620c5136154c35695d0fcde443894f5a3b444c74
suxiaorui <aaa1097395576@foxmail.com>
2023-07-20 00:09:00 +0800

    补充枚举

--- common/src/main/java/com/rui/grace/result/ResponseStatusEnum.java
+++ common/src/main/java/com/rui/grace/result/ResponseStatusEnum.java
@@ -77,8 +77,10 @@ public enum ResponseStatusEnum {
     // 系统错误，未预期的错误 555
     SYSTEM_ERROR(555, false, "系统繁忙，请稍后再试！"),
     SYSTEM_OPERATION_ERROR(556, false, "操作失败，请重试或联系管理员"),
-    SYSTEM_RESPONSE_NO_INFO(557, false, "");
-
+    SYSTEM_RESPONSE_NO_INFO(557, false, ""),
+    SYSTEM_ERROR_GLOBAL(558, false, "全局降级：系统繁忙，请稍后再试！"),
+    SYSTEM_ERROR_FEIGN(559, false, "客户端Feign降级：系统繁忙，请稍后再试！"),
+    SYSTEM_ERROR_ZUUL(560, false, "请求系统过于繁忙，请稍后再试！");
 
     // 响应业务状态
     private Integer status;
