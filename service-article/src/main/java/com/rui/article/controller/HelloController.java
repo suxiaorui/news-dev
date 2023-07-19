d01f3536582fc43b9a47f91f650379e7f8d10130
suxiaorui <aaa1097395576@foxmail.com>
2023-07-19 15:43:24 +0800

    集成Rabbitmq,创建生产者,配置路由规则

--- service-article/src/main/java/com/rui/article/controller/HelloController.java
+++ service-article/src/main/java/com/rui/article/controller/HelloController.java
@@ -1,9 +1,14 @@
 package com.rui.article.controller;
 
+import com.rui.api.config.RabbitMQConfig;
 import com.rui.api.controller.user.HelloControllerApi;
 import com.rui.grace.result.GraceJSONResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,11 +19,54 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
-public class HelloController implements HelloControllerApi {
+@RequestMapping("producer")
+public class HelloController {
 
-    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/hello")
     public Object hello() {
+
+        /**
+         * RabbitMQ 的路由规则 routing key
+         *  display.*.* -> * 代表一个占位符
+         *      例：
+         *          display.do.download  匹配
+         *          display.do.upload.done   不匹配
+         *
+         * display.# -> # 代表任意多个占位符
+         *      例:
+         *          display.do.download  匹配
+         *          display.do.upload.done.over   匹配
+         *
+         *
+         */
+
+
+//        rabbitTemplate.convertAndSend(
+//            RabbitMQConfig.EXCHANGE_ARTICLE,
+//            "article.hello",
+//            "这是从生产者发送的消息~");
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_ARTICLE,
+                "article.publish.download.do",
+                "1001");
+
+
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_ARTICLE,
+                "article.success.do",
+                "1002");
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConfig.EXCHANGE_ARTICLE,
+                "article.play",
+                "1003");
+
         return GraceJSONResult.ok();
     }
 }
