4c91e0f6d705ede0b6f1acfa9651d7f2470d2bfe
suxiaorui <aaa1097395576@foxmail.com>
2023-07-19 18:08:46 +0800

    构建了Eureka注册服务

---
+++ springcloud-eureka/src/main/java/com/rui/eueka/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.rui.eueka.controller;
+
+import com.rui.api.controller.user.HelloControllerApi;
+import com.rui.grace.result.GraceJSONResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author suxiaorui
+ * @Description TODO
+ * @Date 2023/7/19 17:45
+ * @Version 1.0
+ */
+
+
+@RestController
+public class HelloController implements HelloControllerApi {
+
+    final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    public Object hello() {
+        return GraceJSONResult.ok();
+    }
+
+}
