1169f32a426b762f934442dd7a092c9f0c5b61e1
suxiaorui <aaa1097395576@foxmail.com>
2023-07-19 20:08:37 +0800

    补全

--- service-api/src/main/java/com/rui/api/config/Swagger2.java
+++ service-api/src/main/java/com/rui/api/config/Swagger2.java
@@ -33,14 +33,14 @@ public class Swagger2 {
     @Bean
     public Docket createRestApi() {
         Predicate<RequestHandler> adminPredicate = RequestHandlerSelectors.basePackage("com.rui.admin.controller");
-//        Predicate<RequestHandler> articlePredicate = RequestHandlerSelectors.basePackage("com.rui.article.controller");
+        Predicate<RequestHandler> articlePredicate = RequestHandlerSelectors.basePackage("com.rui.article.controller");
         Predicate<RequestHandler> userPredicate = RequestHandlerSelectors.basePackage("com.rui.user.controller");
         Predicate<RequestHandler> filesPredicate = RequestHandlerSelectors.basePackage("com.rui.files.controller");
 
         return new Docket(DocumentationType.SWAGGER_2)  // 指定api类型为swagger2
                 .apiInfo(apiInfo())                 // 用于定义api文档汇总信息
                 .select()
-                .apis(Predicates.or(userPredicate,adminPredicate,filesPredicate))
+                .apis(Predicates.or(userPredicate,articlePredicate, adminPredicate,filesPredicate))
 //                .apis(Predicates.or(adminPredicate, articlePredicate, userPredicate, filesPredicate))
                 .paths(PathSelectors.any())         // 所有controller
                 .build();
