3f08fbb3f15473032e433384b5956b5cb4876f84
suxiaorui <aaa1097395576@foxmail.com>
2023-07-21 09:11:59 +0800

    hystrix——配置自动触发熔断隔离与恢复

--- service-user/src/main/java/com/rui/user/controller/UserController.java
+++ service-user/src/main/java/com/rui/user/controller/UserController.java
@@ -146,6 +146,16 @@ public class UserController extends BaseController implements UserControllerApi 
 
         List<AppUserVO> publisherList = new ArrayList<>();
         List<String> userIdList = JsonUtils.jsonToList(userIds, String.class);
+
+        // FIXME: 仅用于dev测试，硬编码动态判断来抛出异常
+        if (userIdList.size() > 1) {
+            System.out.println("出现异常~~");
+            throw new RuntimeException("出现异常~~");
+        }
+
+
+
+
         for (String userId : userIdList) {
             // 获得用户基本信息
             AppUserVO userVO = getBasicUserInfo(userId);
