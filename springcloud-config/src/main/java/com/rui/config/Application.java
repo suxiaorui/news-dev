c3b315709a8977514492b1b803c7e17bd7a4e2f8
suxiaorui <aaa1097395576@foxmail.com>
2023-07-21 13:58:16 +0800

    config——配置中心实现git配置读取

--- springcloud-config/src/main/java/com/rui/config/Application.java
+++ springcloud-config/src/main/java/com/rui/config/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -20,6 +21,7 @@ import org.springframework.context.annotation.ComponentScan;
         MongoAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.rui", "org.n3r.idworker"})
 @EnableEurekaClient
+@EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
